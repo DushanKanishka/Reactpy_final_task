5f3daa53ad72438585f7fc7b80b1fc1e38fd0100
DushanKanishka <140494829+DushanKanishka@users.noreply.github.com>
2023-08-22 13:30:52 +0530

    update

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-from os import name
-from ssl import _PasswordType
 from fastapi import FastAPI
 from reactpy.backend.fastapi import configure
-from reactpy import component,event, html, use_state
-import reactpy as rp 
+from reactpy import component, event, html, use_state
+import reactpy as rp
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 
 
 @component
 def MyCrud():
-    ##creating state
+    ## Creating state
     alltodo = use_state([])
     name, set_name = use_state("")
     password, set_password = use_state(0)
 
     def mysubmit(event):
-        newtodo ={"name":name,"password":password}
+        newtodo = {"name": name, "password": password}
 
-        #push this to alltodo
-        alltodo.set_value(alltodo.value+[newtodo])
-        login(newtodo)#function call to login function using the submitted data
+        # push this to alltodo
+        alltodo.set_value(alltodo.value + [newtodo])
+        login(newtodo)  # function call to login function using the submitted data
 
-        #looping data from alltodo to show on web 
+    # looping data from alltodo to show on web
 
-        list = [
-            html.li(
+    list = [
+        html.li(
             {
-
+              
             },
-            f"{b}=>{i['name']};{i['password']}",
-            )
-            for b, i in enumerate(alltodo.alltodo.value)
-        ]
-        def handle_event(event):
-            print(event)
+            f"{b} => {i['name']} ; {i['password']} ",
+        )
+        for b, i in enumerate(alltodo.value)
+    ]
 
-        return html.div(
+    def handle_event(event):
+        print(event)
+
+    return html.div(
         {"style": {"padding": "10px"}},
         ## creating form for submission\
         html.form(
@@ -73,3 +72,19 @@ def MyCrud():
 
 app = FastAPI()
 
+from pymongo import MongoClient
+
+#copy and pasting the mongodb URI
+uri= "mongodb+srv://ADMIN:admin123@cluster0.gsgo2ue.mongodb.net/"
+client=MongoClient (uri) #camel case
+db= client ["Reactpy_Task01"] # enter the db name
+Collection = db["Dushan"]
+
+
+#Checking the connection 
+try:
+    client.admin.command("Ping")
+    print("Sucessfully connected Mongodb")
+
+except Exception as e:
+        print(e)
